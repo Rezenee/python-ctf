28ead5a791e64a3d233b4ab989f5487c5a81f15c
Rezenee <zane.rickert@gmail.com>
2020-04-23 19:03:59 -0400

    remove sprite.sprite add x collision

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame as pg
 import time
 import os
-res = x_res, y_res = 1920, 1080
+res = x_res, y_res = 1024, 768
 center_x = x_res / 2
 center_y = y_res / 2
 pg.init()
@@ -24,21 +24,17 @@ red = (255, 0, 0)
 
 class player(pg.sprite.Sprite):
     def __init__(self):
-        pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load(os.path.join("images", "man.png")).convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 0
+        self.x = 0
+        self.y = 0
         self.x_next = 0
         self.y_next = 0
         self.x_vel = 0
         self.y_vel = 0
         self.length = 25
         self.height = 50
-        self.x_offset = (x_res / 2) - (self.length / 2) - self.rect.x
-        self.y_offset = (y_res / 2) + (self.height / 2) + self.rect.y
-
-
+        self.x_offset = (x_res / 2) - (self.length / 2) - self.x
+        self.y_offset = (y_res / 2) + (self.height / 2) + self.y
     def move(self, direction):
         if 1 > self.x_vel > -1:
             if direction == "left":
@@ -48,11 +44,12 @@ class player(pg.sprite.Sprite):
 
     def movement(self):
         for i in walls:
-            screen.blit(rainbow, (walls[i].rect.x + player1.x_offset, walls[i].rect.y + player1.y_offset),
-                        (walls[i].rect.x + player1.x_offset, walls[i].rect.y + player1.y_offset, walls[i].l, walls[i].h))
-        self.x_next = self.rect.x + self.x_vel * dt
+            screen.blit(rainbow, (walls[i].x + player1.x_offset, walls[i].y + player1.y_offset),
+                        (walls[i].x + player1.x_offset, walls[i].y + player1.y_offset, walls[i].l, walls[i].h))
+        self.x_next = self.x + self.x_vel * dt
         self.terrain_collision()
-        self.rect.x += self.x_vel * dt
+        self.x += self.x_vel * dt
+
         # FRICTION
         if self.x_vel > 0:
             self.x_vel = self.x_vel - .05
@@ -60,23 +57,30 @@ class player(pg.sprite.Sprite):
             self.x_vel = self.x_vel + .05
         if .05 > self.x_vel > -.05:
             self.x_vel = 0
-        self.x_offset = (x_res / 2) - (self.length / 2) - self.rect.x
-        self.y_offset = (y_res / 2) + (self.height / 2) - self.rect.y
+        self.x_offset = (x_res / 2) - (self.length / 2) - self.x
+        self.y_offset = (y_res / 2) + (self.height / 2) - self.y
 
     def terrain_collision(self):
-        pass
+     # X COLLISION
+        for wall in walls:
+            print(self.y, 'self.y')
+            print(walls[wall].y + walls[wall].h + self.height)
+            # Moving into the right side of block
+            if walls[wall].x + walls[wall].l > self.x_next > walls[wall].x and walls[wall].y + walls[wall].h + self.height > self.y > walls[wall].y:
+                self.x_vel = 0
+                self.x = walls[wall].x + walls[wall].l
+            # Moving into left side of block
+            if walls[wall].x < self.x_next + self.length < walls[wall].x + walls[wall].l and walls[wall].y + walls[wall].h + self.height > self.y > walls[wall].y:
+                self.x_vel = 0
+                self.x = walls[wall].x - self.length
 
 player1 = player()
 
 
-class block(pg.sprite.Sprite):
+class block:
     def __init__(self, x, y, length, height, colour):
-        pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface([length, height])
-        self.image.fill(colour)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.x = x
+        self.y = y
         self.l = length
         self.h = height
         self.colour = colour
@@ -84,10 +88,9 @@ class block(pg.sprite.Sprite):
 
 def update():
     screen.blit(player1.image, (center_x - player1.length / 2, center_y - player1.height / 2))
-    # for i in walls:
-    #     pg.draw.rect(screen, walls[i].colour, (walls[i].x + player1.x_offset, walls[i].y + player1.y_offset, walls[i].l, walls[i].h))
     for i in walls:
-        screen.blit(walls[i].image, (walls[i].rect.x + player1.x_offset, walls[i].rect.y + player1.y_offset, walls[i].l, walls[i].h))
+        pg.draw.rect(screen, walls[i].colour, (walls[i].x + player1.x_offset, walls[i].y + player1.y_offset, walls[i].l, walls[i].h))
+
     pg.display.update()
 
 
@@ -99,16 +102,17 @@ def initial_draw():
 initial_draw()
 
 walls = {
-        'ground': block(0, 0, 100, 20, grey),
-        'wall': block(-25, -50, 25, 25, grey)
+        # 'ground': block(0, 0, 100, 20, grey),
+        'wall': block(-25, -1, 25, 25, grey),
+        'wall2': block(50, -74,25, 25, grey)
          }
 
 
 while 1:
-    dt = clock.tick(0)
+    dt = clock.tick(60)
 
     keys = pg.key.get_pressed()
-    print(clock.get_fps())
+    #print(clock.get_fps())
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
@@ -129,4 +133,3 @@ while 1:
 
     player1.movement()
     update()
-
