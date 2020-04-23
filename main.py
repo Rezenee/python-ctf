e79d55c014e243dc8245dca51344322f798926e9
Rezenee <zane.rickert@gmail.com>
2020-04-23 15:47:44 -0400

    add sprite.sprite

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ fullscreen_check = 0
 # SPRITES
 background = pg.image.load(os.path.join("images", "wallpaper.jpg")).convert()
 background = pg.transform.scale(background, (x_res, y_res))
-player_sprite = pg.image.load(os.path.join("images", "man.png")).convert_alpha()
 rainbow = pg.image.load(os.path.join("images", "rainbow-fade.jpg")).convert()
 rainbow = pg.transform.scale(rainbow, (x_res,y_res))
 
@@ -26,19 +25,21 @@ red = (255, 0, 0)
 class player(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.x = 0
-        self.y = 0
+        self.image = pg.image.load(os.path.join("images", "man.png")).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
         self.x_next = 0
         self.y_next = 0
         self.x_vel = 0
         self.y_vel = 0
         self.length = 25
         self.height = 50
-        self.x_offset = (x_res / 2) - (self.length / 2) - self.x
-        self.y_offset = (y_res / 2) + (self.height / 2) + self.y
-        self.rect = pg.rect(self.x, self.y, self.length, self.height)
+        self.x_offset = (x_res / 2) - (self.length / 2) - self.rect.x
+        self.y_offset = (y_res / 2) + (self.height / 2) + self.rect.y
+
+
     def move(self, direction):
-        print(self.rect)
         if 1 > self.x_vel > -1:
             if direction == "left":
                 self.x_vel -= .2
@@ -47,12 +48,11 @@ class player(pg.sprite.Sprite):
 
     def movement(self):
         for i in walls:
-            screen.blit(rainbow, (walls[i].x + player1.x_offset, walls[i].y + player1.y_offset),
-                        (walls[i].x + player1.x_offset, walls[i].y + player1.y_offset, walls[i].l, walls[i].h))
-        self.x_next = self.x + self.x_vel * dt
+            screen.blit(rainbow, (walls[i].rect.x + player1.x_offset, walls[i].rect.y + player1.y_offset),
+                        (walls[i].rect.x + player1.x_offset, walls[i].rect.y + player1.y_offset, walls[i].l, walls[i].h))
+        self.x_next = self.rect.x + self.x_vel * dt
         self.terrain_collision()
-        self.x += self.x_vel * dt
-
+        self.rect.x += self.x_vel * dt
         # FRICTION
         if self.x_vel > 0:
             self.x_vel = self.x_vel - .05
@@ -60,8 +60,8 @@ class player(pg.sprite.Sprite):
             self.x_vel = self.x_vel + .05
         if .05 > self.x_vel > -.05:
             self.x_vel = 0
-        self.x_offset = (x_res / 2) - (self.length / 2) - self.x
-        self.y_offset = (y_res / 2) + (self.height / 2) - self.y
+        self.x_offset = (x_res / 2) - (self.length / 2) - self.rect.x
+        self.y_offset = (y_res / 2) + (self.height / 2) - self.rect.y
 
     def terrain_collision(self):
         pass
@@ -69,20 +69,25 @@ class player(pg.sprite.Sprite):
 player1 = player()
 
 
-class block:
+class block(pg.sprite.Sprite):
     def __init__(self, x, y, length, height, colour):
-        self.x = x
-        self.y = y
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface([length, height])
+        self.image.fill(colour)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
         self.l = length
         self.h = height
         self.colour = colour
 
 
 def update():
-    screen.blit(player_sprite, (center_x - player1.length / 2, center_y - player1.height / 2))
+    screen.blit(player1.image, (center_x - player1.length / 2, center_y - player1.height / 2))
+    # for i in walls:
+    #     pg.draw.rect(screen, walls[i].colour, (walls[i].x + player1.x_offset, walls[i].y + player1.y_offset, walls[i].l, walls[i].h))
     for i in walls:
-        pg.draw.rect(screen, walls[i].colour, (walls[i].x + player1.x_offset, walls[i].y + player1.y_offset, walls[i].l, walls[i].h))
-
+        screen.blit(walls[i].image, (walls[i].rect.x + player1.x_offset, walls[i].rect.y + player1.y_offset, walls[i].l, walls[i].h))
     pg.display.update()
 
 
@@ -103,7 +108,7 @@ while 1:
     dt = clock.tick(0)
 
     keys = pg.key.get_pressed()
-    #print(clock.get_fps())
+    print(clock.get_fps())
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
